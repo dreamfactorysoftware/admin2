a604a10a9c616354c0b6af0b0563ac49241915e3
Michael Mandato <michaelmandato@dreamfactory.com>
2015-01-29 16:18:43 -0500

    Bump version.  New build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,7 +39,7 @@ angular
     ])
 
     // Set application version number
-    .constant('APP_VERSION', '1.0.2')
+    .constant('APP_VERSION', '1.0.3')
 
     // Set global url for this application
     .constant('DSP_URL', '')
